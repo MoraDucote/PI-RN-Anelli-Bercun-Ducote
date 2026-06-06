710468c011ea31df83f22b847288a0ef3a7c8ec2
sbercun <sbercun@udesa.edu.ar>
2026-06-06 15:38:35 -0300

    Hice la navegacion Stack y el archivo de home

---
+++ src/navigations/navegacionStack/NavegacionStack.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "../../screens/login/Login";
+import Register from "../../screens/register/Register";
+import NavegacionTab from "../navegacionTab/NavegacionTab";
+
+const Stack = createNativeStackNavigator();
+
+function NavegacionStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen 
+          name="Login" 
+          component={Login} 
+        />
+
+        <Stack.Screen 
+          name="Register" 
+          component={Register} 
+        />
+
+        <Stack.Screen 
+          name="NavegacionTab" 
+          component={NavegacionTab} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default NavegacionStack;
