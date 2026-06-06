36eabbc8250bb0378f2181a865c3cae2b5831f08
sbercun <sbercun@udesa.edu.ar>
2026-06-06 16:25:04 -0300

    Arranque con NewPost.js

--- src/screens/newPost/NewPost.js
+++ src/screens/newPost/NewPost.js
@@ -1,10 +1,43 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, Pressable, StyleSheet } from "react-native";
+import { auth, db } from "../../firebase/config";
+
 
 function NewPost() {
+  const [descripcionPost, setDescripcionPost] = useState("");
+
+  function onSubmit() {
+    db.collection("posts").add({
+      descripcionPost: descripcionPost,
+      email: auth.currentUser.email,
+      createdAt: Date.now(),
+      likes: []
+    })
+    .then(() => {
+      console.log("Post creado correctamente");
+      setDescripcionPost("");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Nuevo post</Text>
+      <Text>Crear nuevo post</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Escribí la descripción del post"
+        keyboardType="default"
+        onChangeText={(text) => setDescripcionPost(text)}
+        value={descripcionPost}
+        multiline={true}
+      />
+
+      <Pressable onPress={() => onSubmit()}>
+        <Text>Crear post</Text>
+      </Pressable>
     </View>
   );
 }
