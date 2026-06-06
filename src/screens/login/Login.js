93d439e8a7108774f60328ac16a980dbab8b4991
MoraDucote <mora@Moras-MacBook-Air-2.local>
2026-06-06 15:59:26 -0300

    descargue bottom tabs

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -12,7 +12,9 @@ function Login() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default Login;
